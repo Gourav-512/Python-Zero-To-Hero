535a8d7a0aebdead5a9ef7965a3155a3ebca3375
Gourav Salunkhe <137747550+Gourav-512@users.noreply.github.com>
2026-06-04 02:44:15 +0530

    Add set examples for unique numbers and skills

--- dict_sets.py
+++ dict_sets.py
@@ -20,10 +20,12 @@ student['city'] = 'Baramati'
 print('Updated:', student)
 
 # Set examples
+
 unique_nums = {1, 2, 2, 3, 4, 4, 5}
 print('Unique set:', unique_nums)
 
 skills1 = {'Python', 'Django'}
 skills2 = {'Django', 'FastAPI', 'SQL'}
+
 print('Union:', skills1 | skills2)
 print('Intersection:', skills1 & skills2)
